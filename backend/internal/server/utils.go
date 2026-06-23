81985b4732e1975eb1a50f8d7b90bfa386916fe5
Daniel Ruijs <daniel.ruijs@gmail.com>
2026-06-23 15:10:44 +0200

    refactor: add additional linters to golangci configuration

--- backend/internal/server/utils.go
+++ backend/internal/server/utils.go
@@ -8,11 +8,11 @@ import (
 func moveDataToMove(data MoveData) (chess.Move, error) {
 	from, err := chess.StrToSquare(data.From)
 	if err != nil {
-		return chess.Move{}, fmt.Errorf("invalid from square: %v", err)
+		return chess.Move{}, fmt.Errorf("invalid from square: %w", err)
 	}
 	to, err := chess.StrToSquare(data.To)
 	if err != nil {
-		return chess.Move{}, fmt.Errorf("invalid to square: %v", err)
+		return chess.Move{}, fmt.Errorf("invalid to square: %w", err)
 	}
 
 	return chess.Move{
