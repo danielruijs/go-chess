2406aa50bee62490c66a23892c462db48756640c
Daniel Ruijs <daniel.ruijs@gmail.com>
2026-08-21 19:09:16 +0200

    fix: unexport api handler

--- backend/internal/api/handler.go
+++ backend/internal/api/handler.go
@@ -8,27 +8,27 @@ import (
 	"go-chess/internal/db/sqlc"
 )
 
-type Handler struct {
+type handler struct {
 	matchHandler *matchHandler
 	userHandler  *userHandler
 }
 
-func NewHandler(ctx context.Context, queries *sqlc.Queries) (*Handler, error) {
+func NewHandler(ctx context.Context, queries *sqlc.Queries) (*handler, error) {
 	matchHandler, err := newMatchHandler(ctx, queries)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create match handler: %w", err)
 	}
 
-	return &Handler{
+	return &handler{
 		matchHandler: matchHandler,
 		userHandler:  newUserHandler(queries),
 	}, nil
 }
 
-func (h *Handler) GetMatch(w http.ResponseWriter, r *http.Request) {
+func (h *handler) GetMatch(w http.ResponseWriter, r *http.Request) {
 	h.matchHandler.getMatch(w, r)
 }
 
-func (h *Handler) GetUserProfile(w http.ResponseWriter, r *http.Request) {
+func (h *handler) GetUserProfile(w http.ResponseWriter, r *http.Request) {
 	h.userHandler.getUserProfile(w, r)
 }
