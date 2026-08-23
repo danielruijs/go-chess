1f0302935174d763f33b33f784da7918502d5848
Daniel Ruijs <daniel.ruijs@gmail.com>
2026-08-23 14:10:37 +0200

    fix: format

--- frontend/src/utils/text.ts
+++ frontend/src/utils/text.ts
@@ -1,9 +1,4 @@
-function pluralize(
-  count: number,
-  singular: string,
-  plural: string,
-  includeCount: boolean,
-): string {
+function pluralize(count: number, singular: string, plural: string, includeCount: boolean): string {
   const word = count === 1 ? singular : plural;
   return includeCount ? `${count} ${word}` : word;
 }
